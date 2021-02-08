1b9c21a45ce5ae69dd5b8ba9d0764d4b8db354bd
arunhari0896 <arunhari0896@gmail.com>
2021-02-08 09:02:27 +0530

    My Second Commit

---
+++ MyProject/src/CheckingTeenager.java
@@ -0,0 +1,23 @@
+
+
+public class CheckingTeenager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ageCheck(23, 19, 87);
+
+	}
+
+	public static void ageCheck(int age1, int age2, int age3) {
+
+		if ((age1 >= 13) && (age1 <= 19) || (age2 >= 13) && (age2 <= 19) || (age3 >= 13) && (age3 <= 19)) {
+			System.out.println("We have a teenager in the group");
+
+		} else {
+			System.out.println("There is no teenager in the group");
+		}
+
+	}
+
+}
